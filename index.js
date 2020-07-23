ba53bc7cafaa8761ba80c34df8147779a1a311b5
Léo SENIQUE <leo.senique@gmail.com>
2020-07-23 18:16:33 +0200

    ajout TP 4

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function fetchUsers() {
+    var table_html = "";
     fetch('https://loisirs-web-backend.cleverapps.io/users')
-        .then(res => res.json())
-        .then((out) => {
-            console.log('Output: ', out);
-        }).catch(err => console.error(err));
+        .then(function (response) { return response.json(); })
+        .then(function (data) {
+            data.forEach(d => {
+                table_html += "<tr><td>" + d.id + "</td><td>" + d.name + "</td><td>" + d.password + "</td></tr>";
+            });
+            document.getElementById('table_users').innerHTML = table_html;
+        })
+        .catch(error => console.error(error));
+}
+
+fetchUsers();
+
+function postUser() {
+    nom = document.getElementById('nom_personne').value;
+    pass = document.getElementById('password').value;
+    //data = document.forms.user_form
+    fetch('https://loisirs-web-backend.cleverapps.io/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: nom, password: pass}) // body data type must match "Content-Type" header
+    }).catch(err => console.error(err));
 }
